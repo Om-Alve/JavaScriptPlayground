1c0e068fe416bb9f2d6cc94a47c6e36ce5166b5f
Om-Alve <omalve8174@gmail.com>
2024-03-23 11:58:48 +0530

    Clock

---
+++ clock/script.js
@@ -0,0 +1,12 @@
+
+function updateTime(){
+    const now = new Date();
+    const time = document.getElementById('time');
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const seconds = now.getSeconds().toString().padStart(2,0);
+    const hours = (now.getHours < 12) ? now.getHours().toString().padStart(2,0) : (now.getHours() % 12).toString().padStart(2,0);
+    const merdian =  (now.getHours < 12) ? 'AM' : 'PM';
+    time.textContent = `${hours} : ${minutes} : ${seconds} ${merdian}`;
+}
+
+setInterval(updateTime,100);
--- diceRoller/script.js
+++ diceRoller/script.js
@@ -7,7 +7,7 @@ rollbtn.onclick = function(){
     let rolls = [];
     result.innerHTML="";
     for(let i=0;i<Number(num_dice.value);i++){
-        rolls.push(Math.floor(Math.random() * 5) + 1);
+        rolls.push(Math.floor(Math.random() * 6) + 1);
         result.innerHTML+= `
         <img src = 'dice/Dice-${rolls[i]}.svg.png' height=100px width=100px style="padding:10px">
         `;
